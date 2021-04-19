be51ec035cf55b596bb8a906515944bc904f602f
Joycezhaoby <bingyuezhao@gmail.com>
2021-04-19 02:45:57 -0400

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,9 +1,10 @@
 from gpiozero import DistanceSensor
-# import RPi.GPIO
 from time import sleep
 
-# pin16 - GPIO23, pin18 - GPIO24
-sensor = DistanceSensor(23, 24) #(echo, trig)
+# pin16-GPIO23, pin18-GPIO24, voltage divider required for echo
+sensor = DistanceSensor(echo=23, trigger=24, max_distance=3)
+sensor.threshold_distance = 0.5
+
 
 while True:
     print('Distance to nearest object is', sensor.distance, 'm')
