0dbfa22f7fd939cd931d1141386d29c966c991bf
Joycezhaoby <bingyuezhao@gmail.com>
2021-04-19 15:00:04 -0400

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -3,9 +3,18 @@ from time import sleep
 
 # pin16-GPIO23, pin18-GPIO24, voltage divider required for echo
 sensor = DistanceSensor(echo=23, trigger=24, max_distance=3)
-sensor.threshold_distance = 0.5
+sensor.threshold_distance = 0.5 #set trigger threshold
 
+def user_absent():
+    print("user absent")
+    #lock screen commands
 
-while True:
-    print('Distance to nearest object is', sensor.distance, 'm')
-    sleep(1)
+def user_present():
+    print("user present")
+    #active screen
+
+def main():
+    sensor.when_out_of_range = user_absent
+    sensor.when_in_range = user_present
+
+main()
