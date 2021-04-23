bc77ef410150678dcb6f4749452fdb9b19581c45
Joycezhaoby <bingyuezhao@gmail.com>
2021-04-23 09:19:27 -0400

    update GUI png display

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from configparser import ConfigParser
 import requests
-
+from PIL import ImageTk, Image
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
 
@@ -10,9 +10,6 @@ config_file = 'config.ini'
 config = ConfigParser()
 config.read(config_file)
 api_key = config['api_key']['key']
-
-
-
 
 app = Tk()
 app.title("Weather app")
@@ -35,13 +32,16 @@ def get_weather(city):
 		return None
 
 
-
 def search():
+	global img
 	city = city_text.get()
 	weather = get_weather(city)
 	if weather:
 		location_label['text'] = '{}, {}'.format(weather[0], weather[1])
-		image['bitmap'] = 'weather_icons/{}.png'.format(weather[4])
+		imgpath = "weather_icons/{}.png".format(weather[4])
+		img = Image.open(imgpath)
+		img = ImageTk.PhotoImage(img)
+		imagel['image'] = img
 		temp_label['text'] = '{:.2f}°C, {:.2f}°F'.format(weather[2], weather[3])
 		weather_label['text'] = weather[5]
 	else:
@@ -57,8 +57,12 @@ search_button.pack()
 location_label = Label(app, text = '', font = ('bold', 20))
 location_label.pack()
 
-image = Label(app, bitmap = '')
-image.pack()
+img = Image.open("weather_icons/02d.png")
+#img = img.resize((150,150))
+img = ImageTk.PhotoImage(img)
+
+imagel = Label(app, image = '')
+imagel.pack()
 
 temp_label = Label(app, text = '')
 temp_label.pack()
