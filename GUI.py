76f84c8affafe586948891fa03d659922890610a
ConnorPeng <71860175+ConnorPeng@users.noreply.github.com>
2021-04-26 12:36:26 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from configparser import ConfigParser
 import requests
-from PIL import ImageTk, Image
+
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
 
@@ -11,13 +11,13 @@ config = ConfigParser()
 config.read(config_file)
 api_key = config['api_key']['key']
 
+
+
+
 app = Tk()
-app.title("Weather GUI")
-app.config(bg="white")
-app.attributes("-fullscreen", True)
-app.bind("<F11>", lambda event: app.attributes("-fullscreen", not app.attributes("-fullscreen")))
-app.bind("<Escape>", lambda event: app.attributes("-fullscreen", False))
+app.title("Weather app")
 app.geometry('700x350')
+
 
 def get_weather(city):
 	result = requests.get(url.format(city, api_key))
@@ -36,41 +36,47 @@ def get_weather(city):
 		return None
 
 
+
 def search():
-	global img
 	city = city_text.get()
 	weather = get_weather(city)
 	if weather:
 		location_label['text'] = '{}, {}'.format(weather[0], weather[1])
-		imgpath = "weather_icons/{}.png".format(weather[4])
-		img = Image.open(imgpath)
-		img = ImageTk.PhotoImage(img)
-		imagel['image'] = img
+		image['bitmap'] = 'weather_icons/{}.png'.format(weather[4])
 		temp_label['text'] = '{:.2f}°C, {:.2f}°F'.format(weather[2], weather[3])
 		weather_label['text'] = weather[5]
 	else:
 		messagebox.showerror('Error', 'Cannot find city')
 
 city_text = StringVar()
-city_entry = Entry(app, textvariable = city_text)
-city_entry.pack(expand=True)
+city_entry = Entry(app, textvariable = city_text).place(x = 250, y = 30)
 
-search_button = Button(app, text = 'Search Weather', width = 12, command = search,bg="white")
-search_button.pack()
 
-location_label = Label(app, text = '', font = ('bold', 20),bg="white")
-location_label.pack()
+Threshold = StringVar()
+Thres_entry = Entry(app, textvariable = Threshold).place(x = 250, y = 60)
 
-img = Image.open("weather_icons/02d.png")
-#img = img.resize((150,150))
-img = ImageTk.PhotoImage(img)
 
-imagel = Label(app, image = '',bg="white")
-imagel.pack()
 
-temp_label = Label(app, text = '',bg="white")
-temp_label.pack()
 
-weather_label = Label(app, text = '',bg="white")
-weather_label.pack(expand=True)
+user_name = Label(app, text = "City Name").place(x = 150,y = 30) 
+user_password = Label(app, text = "Get alert when tempature is below").place(x = 35,y = 60) 
+degree = Label(app, text = "°C").place(x = 440, y = 60)
+
+
+search_button = Button(app, text = 'Search Weather', width = 10, command = search).place(x = 460, y = 30)
+
+alert_button = Button(app, text = 'set alert', width = 10, command = search).place(x = 460, y = 60)
+
+location_label = Label(app, text = '', font = ('bold', 20))
+location_label.place(x = 275, y = 130)
+
+
+image = Label(app, bitmap = '')
+image.place(x = 275, y = 160)
+
+temp_label = Label(app, text = '')
+temp_label.place(x = 275, y = 250)
+
+weather_label = Label(app, text = '')
+weather_label.place(x = 313, y = 280)
 app.mainloop()
